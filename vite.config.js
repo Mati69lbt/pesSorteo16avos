fc0b4590a264e8452d92d28137e1bf6ccb4d8546
Mati69lbt <mati69_lbt@hotmail.com>
2025-09-19 21:44:18 -0300

    deploy3

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: "/pesSorteo16avos/", // 👈 ruta del repo, con / al inicio y al final
   build: { outDir: "docs" },
 });
